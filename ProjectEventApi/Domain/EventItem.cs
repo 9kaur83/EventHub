3dbe40d56b64d794c316852590bc6dfca3b9226f
9kaur83 <55862564+9kaur83@users.noreply.github.com>
2020-04-04 15:47:28 -0700

    Adding initial project
    This is a startup project for assignment

---
+++ ProjectEventApi/Domain/EventItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectEventApi.Domain
+{
+    public class EventItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Fees { get; set; }
+        public string PictureUrl { get; set; }
+        public int EventTypeId { get; set; }
+        public string EventLocationName { get; set; }
+        public virtual EventType EventType { get; set; }
+        public virtual EventLocation EventLocation { get; set; }
+
+
+
+
+    }
+}
---
+++ ProjectEventApi/Domain/EventLocation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectEventApi.Domain
+{
+    public class EventLocation
+    {
+       public string Name { get; set; }
+    }
+}
