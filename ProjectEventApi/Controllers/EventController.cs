b19cdc62bd5f3211fdd2d6b6ef301ca6efdd12aa
Kavita108 <55924498+Kavita108@users.noreply.github.com>
2020-04-19 17:51:12 -0700

    Update EventController.cs
    Added logic for searching event wrt location & eventType id
    Added logic to look for particular event details

--- ProjectEventApi/Controllers/EventController.cs
+++ ProjectEventApi/Controllers/EventController.cs
@@ -27,18 +27,19 @@ namespace ProjectEventApi.Controllers
         [HttpGet]
         [Route("[action]")]
         public async Task<IActionResult> Items(
-            [FromQuery]string location = "",
-            [FromQuery]int eventTypeId = -1,
+           // [FromQuery]string location = "",
+            //[FromQuery]int eventTypeId = -1,
             [FromQuery]int pageIndex = 0,
             [FromQuery]int pageSize = 2)
         {
+            
             /* code for following cases:
                 - Both location and eventTypeId input provided.
                 - Or only location provided
                 - Or only eventTypeId provided
                 - or Both location and eventTypeId not provided.
            */
-
+           /*
             List<EventItem> items;
             List<EventItem> totalItems;
 
@@ -68,13 +69,14 @@ namespace ProjectEventApi.Controllers
                            .Take(pageSize).ToList();
 
             var itemsCount = totalItems.Count();
-            /*
+            */
+            
             var itemsCount = await _context.EventItems.LongCountAsync();
 
            var items = await _context.EventItems.Skip(pageIndex * pageSize)
                                .Take(pageSize)
                                .ToListAsync();
-            */
+            
 
             items = ChangePictureUrl(items);
 
@@ -87,6 +89,8 @@ namespace ProjectEventApi.Controllers
             };
             return Ok(model);
         }
+
+        /*
 
         //This service looks for particular event details
         [HttpGet]
@@ -101,6 +105,8 @@ namespace ProjectEventApi.Controllers
             var items = ChangePictureUrl(item);
             return Ok(items[0]);
         }
+        */
+
         private List<EventItem> ChangePictureUrl(List<EventItem> items)
         {
             items.ForEach(
