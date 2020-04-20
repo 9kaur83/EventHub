61208a988cb1e631c54d9acaed2137e5c1b62f8f
Kavita108 <55924498+Kavita108@users.noreply.github.com>
2020-04-19 17:46:48 -0700

    Update EventController.cs
    Added logic for searched event wrt location & eventType id
    Added logic for searched event for particular event details

--- ProjectEventApi/Controllers/EventController.cs
+++ ProjectEventApi/Controllers/EventController.cs
@@ -27,14 +27,54 @@ namespace ProjectEventApi.Controllers
         [HttpGet]
         [Route("[action]")]
         public async Task<IActionResult> Items(
+            [FromQuery]string location = "",
+            [FromQuery]int eventTypeId = -1,
             [FromQuery]int pageIndex = 0,
             [FromQuery]int pageSize = 2)
         {
+            /* code for following cases:
+                - Both location and eventTypeId input provided.
+                - Or only location provided
+                - Or only eventTypeId provided
+                - or Both location and eventTypeId not provided.
+           */
+
+            List<EventItem> items;
+            List<EventItem> totalItems;
+
+            if (string.IsNullOrEmpty(location) && eventTypeId == -1)
+            {
+                totalItems = await _context.EventItems.ToListAsync();
+            }
+            else if (string.IsNullOrEmpty(location) && eventTypeId != -1)
+            {
+                totalItems = await _context.EventItems
+                         .Where(i => i.EventTypeId == eventTypeId).ToListAsync();
+            }
+            else if (!string.IsNullOrEmpty(location) && eventTypeId == -1)
+            {
+                totalItems = await _context.EventItems
+                          .Where(i => i.Location == location).ToListAsync();
+            }
+            else
+            {
+                totalItems = await _context.EventItems
+                         .Where(i => i.Location == location && i.EventTypeId == eventTypeId)
+                          .ToListAsync();
+            }
+
+            items = totalItems
+                          .Skip(pageIndex * pageSize)
+                           .Take(pageSize).ToList();
+
+            var itemsCount = totalItems.Count();
+            /*
             var itemsCount = await _context.EventItems.LongCountAsync();
 
            var items = await _context.EventItems.Skip(pageIndex * pageSize)
                                .Take(pageSize)
                                .ToListAsync();
+            */
 
             items = ChangePictureUrl(items);
 
@@ -48,6 +88,19 @@ namespace ProjectEventApi.Controllers
             return Ok(model);
         }
 
+        //This service looks for particular event details
+        [HttpGet]
+        [Route("item")]
+        public async Task<IActionResult> Item(
+            [FromQuery]int eventId)
+        {
+            var item = await _context.EventItems
+                         .Where(i => i.Id == eventId)
+                          .ToListAsync();
+
+            var items = ChangePictureUrl(item);
+            return Ok(items[0]);
+        }
         private List<EventItem> ChangePictureUrl(List<EventItem> items)
         {
             items.ForEach(
